28166d151083b87439daf968794d096fdd94e367
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-09-25 01:28:07 -0400

    non wired commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Sarthak Vashistha on 2022-09-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //result label
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //Event handlers
+    
+
+}
+
