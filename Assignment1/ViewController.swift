c7d7d720bab6fd1ad7e475761b4d2d9d28575225
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-09-25 19:34:12 -0400

    final commit

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -2,8 +2,11 @@
 //  ViewController.swift
 //  Assignment1
 //
-//  Created by Sarthak Vashistha on 2022-09-24.
-//
+//  Created by Name: Sarthak Vashistha on 2022-09-24
+//Student ID: 301245284
+//Name: Neha patel
+//Student ID:301280513
+//This is the UI design of a calculator which has all the buttons in iphone 13 pro for IOS 16.0 in Xcode 14.0
 
 import UIKit
 
