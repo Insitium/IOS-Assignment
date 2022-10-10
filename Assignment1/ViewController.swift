8ae4079f9a2fe91ece57e305418397df56b1969c
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-10-09 22:04:13 -0400

    functional commit

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -10,63 +10,126 @@
 
 import UIKit
 
+enum Operation:String {
+    case Add = "+"
+    case Subtract = "-"
+    case divide = "/"
+    case Multiply = "x"
+    case equal = "="
+    case Null = "Null"
+}
+
 class ViewController: UIViewController {
 
     //result label
     @IBOutlet weak var ResultLabel: UILabel!
-    var result: String = ""
-    
+    var pressedNumber = ""
+        var LHS =  ""
+        var RHS = ""
+        var result = ""
+        var currentOperation:Operation = .Null
+        var percent = "100"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ResultLabel.text = "0"
         // Do any additional setup after loading the view.
     }
     //Event handlers
     
-    @IBAction func OperatorButton_Pressed(_ sender: UIButton) {
+    @IBAction func All_Clear(_ sender: UIButton) {
+        pressedNumber = ""
+                LHS = ""
+                RHS = ""
+                result = ""
+                currentOperation = .Null
+                ResultLabel.text = "0"
     }
-    @IBAction func NumberButton_Pressed(_ sender: UIButton) {
-        let button = sender as UIButton
-        let buttonText = button.titleLabel?.text
-        switch(buttonText){
-        case ".":
-            if(!ResultLabel.text!.contains("."))
-            {
-                ResultLabel.text?.append(buttonText!)
-            }
-        default:
-            if(ResultLabel.text == "0"){
-                ResultLabel.text = buttonText
-            }
-            else{
-                ResultLabel.text?.append(buttonText!)
-            }
-            
+    
+    
+    @IBAction func backButton_Pressed(_ sender: UIButton) {
+        pressedNumber = String(pressedNumber.dropLast(1))
+        ResultLabel.text = pressedNumber
+        
+        if pressedNumber.isEmpty {
+            pressedNumber = "0"
         }
     }
     
-    @IBAction func ExtraButton_Pressed(_ sender: UIButton) {
-        let button = sender as UIButton
-        let buttonText = button.titleLabel?.text
-        switch buttonText {
-        case "C":
-            ResultLabel.text = "0"
-        default:
-            if(ResultLabel.text!.count == 1 || (ResultLabel.text!.count == 2 && ResultLabel.text!.contains(".")))
-            {
-                ResultLabel.text = "0"
-            }
-            else
-            {
-                if(ResultLabel.text!.contains(".")){
-                    ResultLabel.text?.removeLast()
-                    ResultLabel.text?.removeLast()
-                }else{
-                    ResultLabel.text?.removeLast()
-                }
-            }
-        }
+    @IBAction func PlusMinus_Press(_ sender: UIButton) {
+        pressedNumber = String( ResultLabel.text!)
+                    pressedNumber = "-" + pressedNumber
+                    ResultLabel.text = pressedNumber
     }
+    
+    
+    @IBAction func divide(_ sender: UIButton) {
+        operation(operation: .divide)
+    }
+    
+    @IBAction func Multiply(_ sender: UIButton) {
+        operation(operation: .Multiply)
+    }
+    
+    @IBAction func Add(_ sender: UIButton) {
+        operation(operation: .Add)
+    }
+    
+    @IBAction func subtract(_ sender: UIButton) {
+        operation(operation: .Subtract)
+    }
+    
+    @IBAction func Equals(_ sender: UIButton) {
+        operation(operation: currentOperation)
+    }
+    
+    @IBAction func dot(_ sender: UIButton) {
+        if pressedNumber.count <= 7{
+                        pressedNumber += "."
+                        ResultLabel.text = pressedNumber
+                    }
+    }
+    func operation(operation: Operation){
+               if currentOperation != .Null{
+                   if pressedNumber != "" {
+                       RHS = pressedNumber
+                       pressedNumber = ""
+                       if currentOperation == .Add {
+                           result = "\(Double(LHS)! + Double(RHS)!)"
+                       }else if currentOperation == .Subtract{
+                           result = "\(Double(LHS)! - Double(RHS)!)"
+                       }else if currentOperation == .divide{
+                           result = "\(Double(LHS)! / Double(RHS)!)"
+                       }else if currentOperation == .Multiply{
+                           result = "\(Double(LHS)! * Double(RHS)!)"
+                       }else if currentOperation == .equal{
+                           
+                       }
+                       LHS = result
+                       if (Double(result)!.truncatingRemainder(dividingBy: 1) == 0){
+                           result = "\(Int(Double(result)!))"
+                       }
+                       ResultLabel.text = result
+                   }
+                   currentOperation = operation
+               }else {
+                   LHS = pressedNumber
+                   pressedNumber = ""
+                   currentOperation = operation
+               }
+           }
+    
+    
+    @IBAction func NumberButton_Pressed(_ sender: UIButton) {
+        if pressedNumber.count <= 8{
+                        pressedNumber += "\(sender.tag)"
+                        ResultLabel.text = pressedNumber
+                    }
+    }
+    
+        
+        
+        
     
     
     
